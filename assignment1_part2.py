a6ab53035d3e8b1fa20df6e9b3c7c991f45a4546
Adama Konate <112704033+adamakonat@users.noreply.github.com>
2022-09-06 16:45:41 +0000

    assignment1_part2.py

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,10 +1,15 @@
 
 class Book:
-	    def __init__(self, author, title):
-	        pass
-	    
-	    def display(self):
-	        pass
-	
-if __name__ == "__main__":
-	    pass
+    def __init__(self, author="", title=""):
+        self.author = author
+        self.title = title
+
+def display(bookname):
+    print(bookname.title + " written by " + bookname.author)
+
+if __name__ == '__main__':
+    a = Book("John Steinbeck", "Of Mice and Men")
+    b = Book("Harper Lee", "Kill a Mockingbird")
+
+display(a)
+display(b)
